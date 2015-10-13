f73be230c26f1d44f64d7695bc099e8fd8e2379a
Dominic Pellegrino <DJPellegrino@dominics-macbook-air.local>
2015-10-13 10:43:06 -0700

    Initial Commit

---
+++ UITableViewController Challenge Solution/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UITableViewController Challenge Solution
+//
+//  Created by Dominic Pellegrino on 10/13/15.
+//  Copyright © 2015 Dominic Pellegrino. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UITableViewController Challenge Solution/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UITableViewController Challenge Solution
+//
+//  Created by Dominic Pellegrino on 10/13/15.
+//  Copyright © 2015 Dominic Pellegrino. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
